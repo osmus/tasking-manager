8ca5d8227b5f8bc0799adb075166d68d6adbac9d
Ian Dees <ian.dees@gmail.com>
2018-04-24 22:52:04 -0500

    Modify config for osm us

--- server/config.py
+++ server/config.py
@@ -4,9 +4,9 @@ import os
 
 class EnvironmentConfig:
     """ Base class for config that is shared between environments """
-    DEFAULT_CHANGESET_COMMENT = '#hotosm-project'
+    DEFAULT_CHANGESET_COMMENT = '#osmus-tasks'
     # This is the address we'll use as the sender on all auto generated emails
-    EMAIL_FROM_ADDRESS = 'noreply@hotosmmail.org'
+    EMAIL_FROM_ADDRESS = 'ian@openstreetmap.us'
     LOG_LEVEL = logging.ERROR
     # Mapper Level values represent number of OSM changesets
     MAPPER_LEVEL_INTERMEDIATE = 250
@@ -40,7 +40,7 @@ class EnvironmentConfig:
 
 
 class ProdConfig(EnvironmentConfig):
-    APP_BASE_URL = 'https://tasks.hotosm.org'
+    APP_BASE_URL = 'https://tasks.openstreetmap.us'
     API_DOCS_URL = f'{APP_BASE_URL}/api-docs/swagger-ui/index.html?' + \
                    f'url={APP_BASE_URL}/api/docs'
     LOG_DIR = '/var/log/tasking-manager-logs'
